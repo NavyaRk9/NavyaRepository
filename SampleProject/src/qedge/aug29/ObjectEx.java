15c3c24f095523d2e6bae2d6d62000ada843a76f
ramakrishna <ramakrishna@192.168.29.98>
2022-10-31 16:53:25 +0530

    Sample Project

---
+++ SampleProject/src/qedge/aug29/ObjectEx.java
@@ -0,0 +1,25 @@
+package qedge.aug29;
+
+public class ObjectEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Object login[][]=new Object[3][2];
+		login[0][0]="Admin1";
+		login[0][1]=123;
+		login[1][0]="Admin2";
+		login[1][1]=456;
+		login[2][0]="Admin3";
+		login[2][1]=789;
+		for (int i = 0; i < login.length; i++) {
+			for (int j = 0; j < login[i].length; j++) {
+				System.out.print(login[i][j]+"\t");
+				
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
---
+++ SampleProject/src/qedge/aug30/ExceptionDemo.java
@@ -0,0 +1,20 @@
+package qedge.aug30;
+
+public class ExceptionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+		int x=10,y=0,z;
+		z=x/y;
+		System.out.println(z);
+		}
+		catch (Throwable e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+
+}
